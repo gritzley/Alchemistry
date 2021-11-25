66de48e3d772433b3388981837ab4935f97516f4
DESKTOP-ALDTLF3\fritz <fritzlloydw@gmail.com>
2021-11-25 10:04:19 +0100

    [ADD] Tool required scripts

---
+++ Assets/Scripts/Editor/Inspector/ToolCustomEditor.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEditorInternal;
+using System.Collections;
+
+[CustomEditor(typeof(Tool))]
+[CanEditMultipleObjects]
+public class ToolEditor : Editor
+{
+    // Serialized properties for recipe and name
+    SerializedProperty conversionsProp;
+    SerializedProperty prefabProp;
+
+    // Reorderable list for the recipe
+    ReorderableList Conversions;
+
+    // This gets called when the inspector is opened
+    void OnEnable()
+    {
+        // Load properties
+        conversionsProp = serializedObject.FindProperty("Conversions");
+        prefabProp = serializedObject.FindProperty("IngredientPrefab");
+
+        // create the reorderable list
+        Conversions = new ReorderableList(serializedObject, conversionsProp, true, true, true, true);
+
+        // Define how the Unity should display elements in the list by overriding the exposed callback
+        Conversions.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+        {
+            // Get the current element
+            var element = Conversions.serializedProperty.GetArrayElementAtIndex(index);
+
+            // Increase target rect y to move the next object down on screen (rect 0,0 is in top left)
+            rect.y += 2;
+
+            float halfWidth = rect.width / 2;
+
+            // Create a property field for the ingredient
+            EditorGUI.PropertyField (
+                // Set the rectangle for the property field position
+                new Rect(rect.x, rect.y, halfWidth, EditorGUIUtility.singleLineHeight),
+                // Get the serialized property
+                element.FindPropertyRelative("Input"),
+                // no GUI content needed
+                GUIContent.none
+            );
+            // Create a property field for the cooking time
+            EditorGUI.PropertyField (
+                // Set the rectangle for the property field position
+                new Rect(rect.x + halfWidth, rect.y, halfWidth, EditorGUIUtility.singleLineHeight),
+                // Get the serialized Property
+                element.FindPropertyRelative("Output"),
+                // no GUI content needed
+                GUIContent.none
+            );
+        };
+    }
+
+    // This gets called every frame that the inspector is drawn
+    public override void OnInspectorGUI()
+    {
+        // Update the serialized Object. Always do this before working with the object
+        serializedObject.Update();
+
+        // Add a property field for the name to the layout
+        EditorGUILayout.PropertyField(prefabProp);
+
+        // Add the reorderable list to the layout
+        Conversions.DoLayoutList();
+
+        // Apply all changes made to serialized properties
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/Utility/Tool.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Conversion
+    {
+        public Ingredient Input;
+        public Ingredient Output;
+    }
+
+    [SerializeField] private List<Conversion> Conversions;
+    [SerializeField] private GameObject IngredientPrefab;
+
+    public IngredientContainer IngredientContainer;
+    
+    public void ConvertIngredinet()
+    {
+        foreach (Conversion conversion in Conversions)
+        {
+            if (conversion.Input == IngredientContainer.Ingredient)
+            {
+                Transform handTransform = IngredientContainer.gameObject.transform.parent;
+                GameObject newIngredient = Object.Instantiate(IngredientPrefab, handTransform.position, Quaternion.identity);
+                Object.Destroy(IngredientContainer.gameObject);
+                IngredientContainer = newIngredient.GetComponent<IngredientContainer>();
+                Debug.Log("No you aren't");
+                IngredientContainer.Ingredient = conversion.Output;
+                IngredientContainer.gameObject.transform.parent = handTransform;
+                IngredientContainer.gameObject.transform.rotation = handTransform.rotation;
+                return;
+            }
+        }
+    }
+
+}
