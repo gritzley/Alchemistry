667de4ff919bcb7ba63e7d15f71a888975daf488
DESKTOP-ALDTLF3\fritz <fritzlloydw@gmail.com>
2021-12-22 12:55:26 +0100

    [ADD] Shelf :)

---
+++ Assets/Scripts/Scriptable Objects/IngredientDefinition.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ingredient Definiton", menuName = "Ingredient Definiton")]
+public class IngredientDefinition : ScriptableObject
+{
+    public string Name;
+    public bool Consumable;
+}
