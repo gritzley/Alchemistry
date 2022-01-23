2a6466b5c8a8faf215b7e4cd639d0579bddd55d2
DESKTOP-ALDTLF3\fritz <fritzlloydw@gmail.com>
2022-01-24 00:22:38 +0100

    [CHG] Cleared Whitespaces

--- Assets/Scripts/Controller/InputController.cs
+++ Assets/Scripts/Controller/InputController.cs
@@ -14,52 +14,26 @@ public class InputController : MonoBehaviour
         // INPUTS
         if (!inputDisabled)
         {
-            if (Input.GetButtonDown("Forward"))
-            {
-                Debug.Log("Forward");
-                pc.Move(Vector3.forward);
-            }
+            if (Input.GetButtonDown("Forward")) pc.Move(Vector3.forward);
 
-            if (Input.GetButtonDown("Left"))
-            {
-                Debug.Log("Left");
-                pc.Move(Vector3.left);
-            }
+            if (Input.GetButtonDown("Left")) pc.Move(Vector3.left);
 
-            if (Input.GetButtonDown("Back"))
-            {
-                Debug.Log("Back");
-                pc.Move(Vector3.back);
-            }
+            if (Input.GetButtonDown("Back")) pc.Move(Vector3.back);
 
-            if (Input.GetButtonDown("Right"))
-            {
-                Debug.Log("Right");
-                pc.Move(Vector3.right);
-            }
+            if (Input.GetButtonDown("Right")) pc.Move(Vector3.right);
 
             // Handle Mouseclick Event
-            if (Input.GetMouseButtonDown(0))
-            {
-                pc.Interact();
-            }
+            if (Input.GetMouseButtonDown(0)) pc.Interact();
 
-            if (Input.GetButtonDown("Pause"))
-            {
-                Debug.Log("pause button pressed");
-                pc.Paused = !pc.Paused;
-            }
+            if (Input.GetButtonDown("Pause")) pc.Paused = !pc.Paused;
 
             if (Input.GetButtonDown("1"))
-            {
                 if (!GameManager.Instance.CurrentCustomer.CurrentDialogueLine.IsReceivingState)
                     GameManager.Instance.CurrentCustomer.ReceiveAnswer(0);
-            }
+
             if (Input.GetButtonDown("2"))
-            {
                 if (GameManager.Instance.CurrentCustomer.CurrentDialogueLine.HasAnswers)
                     GameManager.Instance.CurrentCustomer.ReceiveAnswer(1);
-            }
         }
     }
 }
