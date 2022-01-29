ec082e2702f7f674ef1ddc39336f16496f2de95a
DESKTOP-ALDTLF3\fritz <fritzlloydw@gmail.com>
2022-01-29 19:47:37 +0100

    [ADD] Added Worksurfaces

---
+++ Assets/Scripts/WorkSurface.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WorkSurface : MonoBehaviour
+{
+
+    void OnMouseDown()
+    {
+        if (PlayerController.Instance.HeldItem != null)
+        {
+            RaycastHit hit;
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit);
+            
+            StartCoroutine(PlaceItem(hit.point));
+        }
+    }
+
+    IEnumerator PlaceItem(Vector3 position)
+    {
+        Pickupable item = PlayerController.Instance.HeldItem;
+        Collider itemCollider = item.GetComponentInChildren<MeshCollider>();
+
+        Vector3 direction;
+        float distance;
+        foreach (Collider collider in GetComponentsInChildren<MeshCollider>())
+        {
+            Debug.Log(collider.transform.position);
+            Debug.Log(position);
+            if (Physics.ComputePenetration(
+                collider, collider.transform.position, transform.rotation * collider.transform.localRotation,
+                itemCollider, position, transform.rotation * itemCollider.transform.localRotation,
+                out direction, out distance))
+                    yield break;
+        }
+        
+        PlayerController.Instance.HeldItem = null;
+        item.transform.LeanRotate(Vector3.zero, 0.15f);
+        item.transform.LeanMove(position, 0.15f);
+        item.transform.parent = transform;
+        yield return new WaitForSeconds(0.15f);
+
+    }
+
+}
